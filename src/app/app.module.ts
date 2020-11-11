af14cbf8781d0b98b544b2bb61bd1a76f1e85404
henrique945 <henrique.rodrigues7@hotmail.com>
2020-11-11 19:43:54 -0300

    update(app.module): add HelperService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+//#region Imports
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Network } from '@ionic-native/network/ngx';
 import { IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { httpAsyncFactory } from './factories/http-async.factory';
+import { HelperService } from './services/helper/helper.service';
 import { HttpAsyncService } from './services/http-async.service.ts/http-async.service';
 import { StorageService } from './services/storage/storage.service';
+
+//#endregion
 
 @NgModule({
   imports: [
@@ -26,7 +32,8 @@ import { StorageService } from './services/storage/storage.service';
     AppComponent
   ],
   providers: [
-    { provide: HttpAsyncService, useFactory: httpAsyncFactory, deps: [HttpClient, StorageService] },
+    { provide: HttpAsyncService, useFactory: httpAsyncFactory, deps: [HttpClient, HelperService, StorageService] },
+    Network,
   ],
   exports: [],
   bootstrap: [AppComponent]
