bc610db7b1798cd288f1337e612f9cd4bffea320
viniciussouzacss <50721086+viniciussouzacss@users.noreply.github.com>
2020-11-21 05:27:14 -0300

    update (home): add components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const textarea = document.querySelector('textarea');
 
+    textarea.addEventListener('keydown', autosize);
+
+    function autosize() {
+      const el = this;
+      setTimeout(function () {
+        el.style.cssText = 'height:auto; padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      }, 0);
+    }
+  }
 }
